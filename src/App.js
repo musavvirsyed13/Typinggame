711a5bc43cafa017c10831f5c3365148feebd616
Syed Musavvir <musavvirsyed13@gmail.com>
2022-05-15 06:10:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Words from "./Components/Words";
+import Container from "./Components/Container";
+import Typeracer from "./Components/Typeracer";
+import Results from "./Components/Results";
+
+const App = () => {
+  const [word, setWord] = useState(Words);
+  const [newWord, setNewWord] = useState(word[0]);
+  const [disabled, setDisabled] = useState(true);
+  const [correctResults, setCorrectResults] = useState([]);
+  const [wrongResults, setWrongResults] = useState([]);
+  const [countCorrect, setCountCorrect] = useState(0);
+  const [time, setTime] = useState(30);
+  const [inputValue, setInputValue] = useState("");
+  const [animation, setAnimation] = useState(null);
+
+  let randomWord = Math.floor(Math.random() * word.length);
+
+  const checkAnswers = () => {
+    if (inputValue.trim() === newWord) {
+      setCorrectResults((prevCorrect) => [...prevCorrect, newWord]);
+      setCountCorrect((prevCorrect) => prevCorrect + 1);
+      return;
+    }
+    setWrongResults((prevWrong) => [...prevWrong, inputValue]);
+  };
+
+  const handleInput = (e) => {
+    if (e.charCode === 13 && inputValue.trim() !== "") {
+      checkAnswers();
+      setNewWord(word[randomWord]);
+      setInputValue("");
+    }
+  };
+
+  const handleStart = () => {
+    setDisabled(!disabled);
+    setCorrectResults([]);
+    setWrongResults([]);
+    setCountCorrect(0);
+    setInputValue("");
+  };
+
+  useEffect(() => {
+    if (time <= 30 && time !== 0 && disabled === false) {
+      setTimeout(() => setTime((prevTime) => prevTime - 1), 1000);
+    } else if (disabled) {
+      setTime(30);
+      setAnimation(null);
+    } else if (time === 0) {
+      setDisabled(true);
+    }
+    if (time <= 10) {
+      setAnimation("scaleNumber 2s infinite");
+    }
+  }, [disabled, time]);
+
+  useEffect(() => {
+    setNewWord(word[randomWord]);
+  }, []);
+
+  return (
+    <div className="App">
+      <Container>
+        <Typeracer
+          newWord={newWord}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          disabled={disabled}
+          time={time}
+          animation={animation}
+          handleInput={handleInput}
+          handleStart={handleStart}
+        />
+      </Container>
+      <Results
+        correctResults={correctResults}
+        wrongResults={wrongResults}
+        countCorrect={countCorrect}
+      />
+    </div>
+  );
+};
+
+export default App;
